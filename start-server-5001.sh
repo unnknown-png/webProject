7bf4fc5c17e5ececb1ade0faaefea3fb8d6222ed
unnknown-png <kahnovets.ap@gmail.com>
2025-12-08 23:33:28 +0200

    Local servers for Nginx balancing testing

--- start-server-5001.sh
+++ start-server-5001.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Server 5001
+# Local servers for Nginx balancing testing
 cd "$(dirname "$0")/webProject"
 echo "Starting Server 5001..."
 dotnet run --urls "http://localhost:5001" --environment "Production" --launch-profile "" -- --ServerInfo:ServerName=SERVER-5001 --ServerInfo:Port=5001
-
